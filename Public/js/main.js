63ccb3ff7389e0519d2288c9b656453a6c9d917c
Abdel <elb.abd@icloud.com>
2021-02-12 23:22:10 +0100

    ajoute commentaire

--- Public/js/main.js
+++ Public/js/main.js
@@ -2,54 +2,64 @@ window.onload=function(){
     canv=document.getElementById("ecran");
     ctx=canv.getContext("2d");
     document.addEventListener("keydown",keyPush);
-    setInterval(game,1000/15);
+    setInterval(game,1000/15); //faire bouger le serpent de 15 toutes les secondes
 }
-px=py=10;
+//position de mon joueur
+positionx=positiony=10;
 gs=tc=20;
+//taille de la pomme que le serpent vas manger
 ax=ay=15;
+
+//vitesse du joueur v
 xv=yv=0;
+
+//corps du joueur
 trail =[],
 tail = 5;
+
+//fonction du jeu 
+
 function game(){
-    px+=xv;
-    py+=yv;
-    if(px<0) {
-        px = tc-1;
+    positionx+=xv;
+    positiony+=yv;
+    if(positionx<0) {
+        positionx = tc-1;
     }
-    if(px> tc-1) {
-        px = 0
+    if(positionx> tc-1) {
+        positionx = 0
     }
-    if(py<0) {
-        py = tc-1;
+    if(positiony<0) {
+        positiony = tc-1;
     }
-    if(py>tc-1) {
-        py = 0;
+    if(positiony>tc-1) {
+        positiony = 0;
     }
     ctx.fillStyle="black";
-    ctx.fillRect(0,0,canv.width,canv.height);
+    ctx.fillRect(0,0,canv.width,canv.height); //couleur du background
 
-    ctx.fillStyle="lime";
-    for(let i=0; i<trail.length; i++){ 
-        ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2);
+    ctx.fillStyle="yellow";
+    for(let i=0; i<trail.length; i++){
+        ctx.fillRect(trail[i].x*gs, trail[i].y*gs, gs-2, gs-2); //couleur du serpent
 
-        if(trail[i].x==px && trail[i].y==py){
-        tail =5;
+        if(trail[i].x==positionx && trail[i].y==positiony){
+        tail =5; //conditions si le joueurs se touche, le jeu se reinitialise il reprends sa forme du debut 
         }
     }
-trail.push({x:px,y:py});
+trail.push({x:positionx,y:positiony});
 while(trail.length>tail){
-trail.shift();
+trail.shift(); // ajoute la pomme a la fin de mon tableau (serpent)
 }
-if(ax==px && ay==py){
+if(ax==positionx && ay==positiony){
     tail++;
     ax=Math.floor(Math.random()*tc);
-    ay=Math.floor(Math.random()*tc);
+    ay=Math.floor(Math.random()*tc);//si il touche la pomme il s'incremente d'un carré
 }
 
     ctx.fillStyle="red";
-    ctx.fillRect(ax*gs, ay*gs, gs-2, gs-2);
+    ctx.fillRect(ax*gs, ay*gs, gs-2, gs-2); //couleur de la pomme
 
 }
+// touche attribuer pour bouger le serpent haut, bas, gauche, droite
 function keyPush(evt){
     switch (evt.keyCode) {
         case 37:
